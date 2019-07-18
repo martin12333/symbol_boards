5294e9e7f38fa295fdaf441daef679fa148ba78f
Martin Novy <martin12333@users.noreply.github.com>
2019-07-18 14:19:24 +0200

    codepen + font

---
+++ script.js
@@ -0,0 +1,43 @@
+$('.keyboard').keyboard({
+	alwaysOpen: true,
+  usePreview: false,
+  appendTo: '#keyboard',
+  position: false,
+  reposition: false,
+  tabNavigation: true
+////});
+, 
+////  // *** Mapped keys ***
+////$('#map').keyboard({
+	layout: 'custom',
+	customLayout: {
+		'normal' : /////[
+			// "n(a):title_or_tooltip"; n = new key, (a) = actual key, ":label" = title_or_tooltip (use an underscore "_" in place of a space " ")
+			//// `:NOT_A_SYMBOL 
+			//// :NOT_A_SYMBOL 
+////// begin layout
+[
+":forward_ :backward_ :upward_ :downward_ :(not_a_symbol) :continuation_ :(not_a_symbol) :through_ :(not_a_symbol) :PART_OF_exchange_ :? :(not_a_symbol) :competition_ :(not_a_symbol) :? :? :? :back_and_forth_ :(not_a_symbol) :upward_and_forward_ :downward_and_forward_ :upward_and_backward_ :downward_and_backward_ :PART_OF_reflection_ :(not_a_symbol) :PART_OF_rotation_ :(not_a_symbol) :PART_OF_turn_ :(not_a_symbol) :jump_ :feeling_ :rest_ :? :? :? :? :PART_OF_? :(not_a_symbol) :",
+":fire_ :water_ :rain_ :? :(not_a_symbol) :snake_ :fish_ :(not_a_symbol) :wing_ :(not_a_symbol) :future_ :past_ :PART_OF_ :PART_OF_around_ :PART_OF_ :PART_OF_ :indicator_ :indicator_ :PART_OF_time_ :(not_a_symbol) :sun_ :day_ :?? :moon_ :earth_ :(not_a_symbol) :field_ :leaf_ :flower_ :(not_a_symbol) :tree_ :(not_a_symbol) :",
+":mind_ :PART_OF_? :? :? :(not_a_symbol) :bowl_ :fullness_ :(not_a_symbol) :getting_ :giving_ :ship_ :bag_ :(not_a_symbol) :? :(not_a_symbol) :mouth_ :name_ :music_ :(not_a_symbol) :eye_ :wakefulness_ :sleep_ :light_ :face_ :ear_ :(not_a_symbol) :combination_ :PART_OF_ :(not_a_symbol) :PART_OF_ :machine_ :airplane_ :(not_a_symbol) :",
+": :life_ :change_ :(not_a_symbol) :money_ :(not_a_symbol) :t :t :(not_a_symbol) :PART_OF_ :t :t :(not_a_symbol) :t :t :t :t :t :t :t :(not_a_symbol) :t :t :t :t :t :t :t :",
+":t :t :t :(not_a_symbol) :t :t :t :(not_a_symbol) :t :t :t :(not_a_symbol) :t :electricity_ :t :t :(not_a_symbol) :t :t :t :(not_a_symbol) :t :t :t :t :t :t :(not_a_symbol) :t :t :t :t :t :(not_a_symbol) :t :t :t :(not_a_symbol) :",
+":t :t :t :t :(not_a_symbol) :t :t :t :(not_a_symbol) :t :t :t :t :(not_a_symbol) :t :t :(not_a_symbol) :t :t :t :(not_a_symbol) :t :t :t :t :(not_a_symbol) :",
+":t :t :t :t :t :t :t :(not_a_symbol) :t :t :ownership_ :t :t :(not_a_symbol) :t :t :t :t :t :t :(not_a_symbol) :t :t :(not_a_symbol) :(not_a_symbol) :(not_a_symbol) :(not_a_symbol) :(not_a_symbol) :",
+":t :t :t :(not_a_symbol) :t :(not_a_symbol) :action_ :t :t :t :(not_a_symbol) :t :t :t :(not_a_symbol) :enclosure_ :t :thing_ :t :t :t :(not_a_symbol) :t :t :(not_a_symbol) :t :(not_a_symbol) :t :(not_a_symbol) :t :t undefined:undefined"
+]
+
+
+////// end layout
+			////'\u00a1(1) \u00a2(2) \u00a3(3) \u00a4(4) \u00a5(5) \u00a6(6)', 
+      ////'\u03b1(a):lower_case_alpha \u03b2(b):lower_case_beta \u03b3(c):lower_case_gamma \u03b4(d):lower_case_delta \u03b5(e):lower_case_epsilon \u03b6(f):lower_case_zeta \u03b7(g):lower_case_eta', // lower case Greek
+			////'{shift} {accept} {cancel}'
+		/////]////,
+		////'shift' : [
+      ////'(Q)',
+			////'\u0391(A) \u0392(B) \u0393(C) \u0394(D) \u0395(E) \u0396(F) \u0397(G)', // upper case Greek
+			////'{shift} {accept} {cancel}'
+		////]
+	} ////,
+
+});
